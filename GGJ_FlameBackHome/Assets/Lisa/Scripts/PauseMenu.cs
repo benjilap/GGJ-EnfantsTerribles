657219a15302dbff73c8af3d8196bdb127c03795
Lisouille69 <boileau.lisa@orange.fr>
2019-01-27 15:49:39 +0100

    UpdatePauseMenu

--- GGJ_FlameBackHome/Assets/Lisa/Scripts/PauseMenu.cs
+++ GGJ_FlameBackHome/Assets/Lisa/Scripts/PauseMenu.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+
+    public GameObject PauseUi;
+    private bool paused = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PauseUi.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            PauseUi.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+
+        if (!paused)
+        {
+            PauseUi.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
